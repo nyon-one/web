a485f5479fbf1d0272d78fc8bfc5d67da2b78067
Axter <one.nyon@gmail.com>
2018-07-22 17:40:12 +0545

    Add setup for easy install

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='www',
+      version='1.1',
+      description='Utilities for Requests module',
+      author='nyon.one',
+      packages=['www'],
+     )

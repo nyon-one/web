e231140046e8af3fad37d31db72fa5d70c825b94
Axter <one.nyon@gmail.com>
2018-07-22 12:00:02 +0545

    init

---
+++ www/__init__.py
@@ -0,0 +1,23 @@
+from requests_html import *
+from pprint import (pformat, pprint)
+import logging
+
+DEBUG = False
+ANDROID_USER_AGENT = {'User-Agent':'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36'}
+
+if DEBUG:logging.basicConfig(level=DEBUG)
+
+log = logging.getLogger(__name__)
+
+def client(use_android_user_agent=True, *args, **kwargs):
+	session = HTMLSession(*args, **kwargs)
+	if use_android_user_agent:
+
+		session.headers.update(ANDROID_USER_AGENT)
+	
+	if 'headers' in kwargs:
+		session.headers.update(kwargs['headers'])
+
+	log.debug(pformat(session.headers))
+	log.debug(pformat(session.cookies))
+	return session

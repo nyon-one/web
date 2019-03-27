268b7f489eb26b9802c3a9bc2d0dcfc01959eb8e
nyon-one <34093497+nyon-one@users.noreply.github.com>
2019-03-27 14:46:02 +0545

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='www',
-      version='1.2.3',
+      version='1.2.4',
       description='Utilities for Requests module',
       author='nyon.one',
       install_requires=['requests-html'],

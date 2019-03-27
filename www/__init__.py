933c41017a55ee4088a5790fd54866cd95d50a14
nyon-one <one.nyon@gmail.com>
2019-03-27 14:36:36 +0545

    Add:browser cookie module

--- www/__init__.py
+++ www/__init__.py
@@ -2,18 +2,25 @@ from requests_html import *
 from pprint import (pformat, pprint)
 import logging
 from pathlib import Path
+import os
+import browser_cookie3 as bc
 
-DEBUG = False
+APPDATA = Path(os.getenv('APPDATA', ''))
+
+def get_chrome_cookies(profile):
+    user = APPDATA.joinpath(fr'..\Local\Google\Chrome\\User Data\{profile}\Cookies')
+    return bc.Chrome(str(user)).load()
+
 ANDROID_USER_AGENT = {'User-Agent':'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36'}
 
-if DEBUG:logging.basicConfig(level=DEBUG)
+def debug():
+	logging.basicConfig(level=logging.DEBUG)
 
 log = logging.getLogger(__name__)
 
 def client(use_android_user_agent=True, *args, **kwargs):
 	session = HTMLSession(*args, **kwargs)
 	if use_android_user_agent:
-
 		session.headers.update(ANDROID_USER_AGENT)
 	
 	if 'headers' in kwargs:
